edcc6b172de7d523087419807a848d993957f9c0
Rok Krhlikar <rok.krhlikar@gmail.com>
2020-03-08 21:16:01 +0100

    [Linux] Move Glade and icon file into assets subdirectory

--- linux/src/main.cpp
+++ linux/src/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char** argv)
 {
     auto app = Gtk::Application::create(argc, argv, "fm.qmk.toolkit");
-    auto builder = Gtk::Builder::create_from_file("main.glade", "main");
+    auto builder = Gtk::Builder::create_from_file("assets/main.glade", "main");
 
     qmk::MainWindow* pMainWindow;
     builder->get_widget_derived("main", pMainWindow);
